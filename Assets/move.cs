6169dd90471fe26db853bc3d76c7b2bffaab0111
Muhammed Ramadan <mohamedr201477@gmail.com>
2017-08-14 16:17:23 +0200

    phase 1

---
+++ Assets/move.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	public WheelCollider rf;
+	public WheelCollider lf;
+	public WheelCollider rb;
+	public WheelCollider lb;
+	public int speed;
+	public int angle;
+
+
+	void Update(){
+		rb.motorTorque = Input.GetAxis("Vertical")* speed ;
+		lb.motorTorque = Input.GetAxis("Vertical")* speed ;
+		rf.steerAngle = Input.GetAxis ("Horizontal")* angle ;
+		lf.steerAngle = Input.GetAxis ("Horizontal")* angle ;
+
+	}
+
+	
+}
---
+++ Assets/sphere.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sphere : MonoBehaviour 
+{
+	TextMesh label;
+	void Start()
+	{
+		label = GetComponent<TextMesh>();
+		print (label.text);
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (col.gameObject.name == "Sphere") {
+			label.text = "Finish";
+			print (label.text);
+		}
+
+	}
+}
